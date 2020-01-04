6f1332e4d9cd912e16a793787872d5f328e312ba
gaodehan <573308611@qq.com>
2020-01-04 18:18:16 +0800

    添加启动类！！！

--- MicroeService-SpringCloud/eureka-client/src/main/java/com/eurekaclient/eurekaclient/EurekaClientApplication.java
+++ MicroeService-SpringCloud/eureka-client/src/main/java/com/eurekaclient/eurekaclient/EurekaClientApplication.java
@@ -13,6 +13,7 @@ public class EurekaClientApplication {
     public static void main(String[] args) {
         SpringApplication.run(EurekaClientApplication.class, args);
         System.out.println("添加启动类!!!");
+        System.out.println("添加启动类!!!");
     }
 
 }
