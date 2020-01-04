e588da8a3cc4a84419008443d6637c15ee1a72d9
gaodehan <573308611@qq.com>
2020-01-04 17:32:39 +0800

    提交代码

--- MicroeService-SpringCloud/eureka-server3/src/main/java/com/eurekaserver3/eurekaserver3/EurekaServer3Application.java
+++ MicroeService-SpringCloud/eureka-server3/src/main/java/com/eurekaserver3/eurekaserver3/EurekaServer3Application.java
@@ -9,8 +9,12 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class EurekaServer3Application {
 
+
     public static void main(String[] args) {
         SpringApplication.run(EurekaServer3Application.class, args);
+        System.out.println("EurekaServer3启动完成！！！！");
+
+        System.out.println("EurekaServer3启动完成！！！！");
     }
 
 }
