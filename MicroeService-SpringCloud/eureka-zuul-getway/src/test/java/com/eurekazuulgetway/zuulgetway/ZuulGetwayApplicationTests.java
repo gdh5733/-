a96c4b14bb50ac2e0630184deeeea9f4ff821d97
gaodehan <573308611@qq.com>
2020-01-05 20:24:31 +0800

    网关建立

---
+++ MicroeService-SpringCloud/eureka-zuul-getway/src/test/java/com/eurekazuulgetway/zuulgetway/ZuulGetwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eurekazuulgetway.zuulgetway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulGetwayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
