bc5a11e5902c4552ceaf629bbf7840ba0847954d
gaodehan <573308611@qq.com>
2020-01-05 19:14:59 +0800

    提交网关项目建立

---
+++ MicroeService-SpringCloud/eureka-zuul-getway/src/main/java/com/eurekazuulgetway/eurekazuulgetway/EurekaZuulGetwayApplication.java
@@ -0,0 +1,13 @@
+package com.eurekazuulgetway.eurekazuulgetway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaZuulGetwayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaZuulGetwayApplication.class, args);
+    }
+
+}
---
+++ MicroeService-SpringCloud/eureka-zuul-getway/src/test/java/com/eurekazuulgetway/eurekazuulgetway/EurekaZuulGetwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eurekazuulgetway.eurekazuulgetway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaZuulGetwayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
